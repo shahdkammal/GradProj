f908e408c9f094aa230bfd0f56577c07637a8d1e
shahdkammal <shahd.a.kamal.9@gmail.com>
2024-11-29 01:57:40 -0800

    delete service back

--- admin/service_delete.php
+++ admin/service_delete.php
@@ -2,19 +2,46 @@
 include '../db/dbconnect.php';
 session_start();
 
-
-$data = false;
 if(isset($_GET['deleteid'])){
     $service_id = $_GET['deleteid'];
-    // echo $service_id;
-    $sql = "DELETE FROM `service` WHERE `service`.`service_id` = $service_id";
-    $result = mysqli_query($conn,$sql);
-    if ($result){
+
+    // Start a transaction
+    $conn->begin_transaction();
+
+    try {
+        // Delete related rows in sp_service table
+        $sql1 = "DELETE FROM `sp_service` WHERE `service_id` = ?";
+        $stmt1 = $conn->prepare($sql1);
+        if ($stmt1) {
+            $stmt1->bind_param("i", $service_id);
+            $stmt1->execute();
+            $stmt1->close();
+        } else {
+            throw new Exception("Error preparing statement for sp_service table: " . $conn->error);
+        }
+
+        // Delete row in service table
+        $sql2 = "DELETE FROM `service` WHERE `service`.`service_id` = ?";
+        $stmt2 = $conn->prepare($sql2);
+        if ($stmt2) {
+            $stmt2->bind_param("i", $service_id);
+            $stmt2->execute();
+            $stmt2->close();
+        } else {
+            throw new Exception("Error preparing statement for service table: " . $conn->error);
+        }
+
+        // Commit the transaction
+        $conn->commit();
         $_SESSION['status'] = "Service is deleted successfully.";
         header("location: service_view.php");
-    }else{
+    } catch (Exception $e) {
+        // Rollback the transaction if there was an error
+        $conn->rollback();
         $_SESSION['statusfail'] = "Sorry, service is not deleted.";
+        echo 'Error: ' . $e->getMessage();
         header("location: service_view.php");
     }
 }
+$conn->close();
 ?>
--- admin/sp_view.php
+++ admin/sp_view.php
@@ -1,25 +1,17 @@
 <?php
 include '../db/dbconnect.php';
-// session_start();
 include 'assets/include/admin_header.php';
 ?>
 
-
-
 <div class="col-sm-9 col-xs-12 content pt-3 pl-0">
-
-
-    <div class="row ">
+    <div class="row">
         <div class="col-lg-5">
-
             <h5 class="mb-0"><strong>Service Provider</strong></h5>
             <span class="text-secondary">Dashboard <i class="fa fa-angle-right"></i> View Service Provider Details</span>
         </div>
-        <div class="col-md-auto col-lg-7">  
-
+        <div class="col-md-auto col-lg-7">
             <!-- Message section -->
             <?php
-            //Alert OR Error Message:
             if (isset($_SESSION['status'])) {
                 echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
                         <strong>Success! </strong> ' . $_SESSION['status'] . '
@@ -30,7 +22,7 @@ include 'assets/include/admin_header.php';
                 unset($_SESSION['status']);
             } elseif (isset($_SESSION['statusfail'])) {
                 echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>Success! </strong> ' . $_SESSION['statusfail'] . '
+                        <strong>Sorry! </strong> ' . $_SESSION['statusfail'] . '
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                         </button>
@@ -38,24 +30,12 @@ include 'assets/include/admin_header.php';
                 unset($_SESSION['statusfail']);
             }
             ?>
-
-
-
-
         </div>
     </div>
 
-
-    <?php
-
-    ?>
-
-
     <div class="row mt-3">
         <div class="col-sm-12">
-            <!--Datatable-->
             <div class="mt-1 mb-3 p-3 button-container bg-white border shadow-sm">
-
                 <div class="table-responsive">
                     <table id="example" class="table table-striped table-bordered">
                         <thead>
@@ -70,15 +50,13 @@ include 'assets/include/admin_header.php';
                             </tr>
                         </thead>
                         <tbody>
-                            <?php // SERVICE PROVIDER VIEW CODE. Data get from SP & CITY table using inner joinn becase we need "city name" from city table.
-                            $sql = "SELECT `sp`.* , `city`.*
-                            FROM `sp` INNER JOIN `city` 
-                            ON `sp`.city_id=`city`.city_id";
+                            <?php
+                            $sql = "SELECT `sp`.*, `city`.* FROM `sp` INNER JOIN `city` ON `sp`.city_id=`city`.city_id";
                             $result = mysqli_query($conn, $sql);
                             if ($result) {
                                 $sno = 0;
                                 while ($row = mysqli_fetch_assoc($result)) {
-                                    $sno = $sno + 1;
+                                    $sno++;
                                     $login_id = $row['login_id'];
                                     $sp_id = $row['sp_id'];
                                     $sp_name = $row['sp_name'];
@@ -87,49 +65,42 @@ include 'assets/include/admin_header.php';
                                     $city = $row['city_name'];
                                     $status = $row['status'];
 
-                            ?>
-                                    <tr>
-                                        <td><?php echo $sno ?></td>
-                                        <td><?php echo $sp_name ?></td>
-                                        <td><?php echo $email ?></td>
-                                        <td><?php echo $phone ?></td>
-                                        <td><?php echo $city ?></td>
-                                        <td>
-                                            <?php
-                                            if ($status == 'deactive') {
-                                                echo '<span class="badge badge-warning">Deactive</span>';
-                                            }
-                                            if ($status == 'active') {
-                                                echo '<span class="badge badge-success">Active</span>';
-                                            }
-                                            ?>
-                                        </td>
-
-                                        <td>
-                                            <?php
-                                            if ($status == 'deactive') {
-                                               ?>
-                                               <a href="approve.php?spid=<?php echo $sp_id ?>&status=<?php echo $status ?>" title="Activate service provider"> <button type="button" class="btn btn-success btn-sm"><i class="fa fa-check"></i></button></a>
-                                               <?php
-                                            }
-                                            if ($status == 'active') {
-                                                ?>
-                                                <a href="approve.php?spid=<?php echo $sp_id ?>&status=<?php echo $status ?>" title="Deactivate service provider"> <button type="button" class="btn btn-danger btn-sm"><i class="fa fa-close"></i></button></a>
-                                                <?php
-                                            }
-                                            ?>
-                                            <!-- <a href="sp_details.php"><button type="submit" class="btn btn-sm btn-theme">See details</button></a> -->
-                                            <a href="sp_delete.php?spid=<?php echo $sp_id ?>&loginid=<?php echo $login_id ?>"><button type="submit" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button></a>
-                                            <a href="sp_gig_view.php?spid=<?php echo $sp_id ?>&spname=<?php echo $sp_name ?>" title="Go to Service gig" ><button type="submit" class="btn btn-sm btn-theme"><i class="fas fa-eye"></i></button></a>
-                                        </td>
-                                    </tr>
-
-                            <?php
+                                    echo '<tr>
+                                            <td>' . $sno . '</td>
+                                            <td>' . $sp_name . '</td>
+                                            <td>' . $email . '</td>
+                                            <td>' . $phone . '</td>
+                                            <td>' . $city . '</td>
+                                            <td>';
+                                    if ($status == 'deactive') {
+                                        echo '<span class="badge badge-warning">Deactive</span>';
+                                    }
+                                    if ($status == 'active') {
+                                        echo '<span class="badge badge-success">Active</span>';
+                                    }
+                                    echo '</td>
+                                            <td>';
+                                    if ($status == 'deactive') {
+                                        echo '<a href="approve.php?spid=' . $sp_id . '&status=' . $status . '" title="Activate service provider">
+                                                <button type="button" class="btn btn-success btn-sm"><i class="fa fa-check"></i></button>
+                                              </a>';
+                                    }
+                                    if ($status == 'active') {
+                                        echo '<a href="approve.php?spid=' . $sp_id . '&status=' . $status . '" title="Deactivate service provider">
+                                                <button type="button" class="btn btn-danger btn-sm"><i class="fa fa-close"></i></button>
+                                              </a>';
+                                    }
+                                    echo '<a href="sp_delete.php?spid=' . $sp_id . '&loginid=' . $login_id . '">
+                                            <button type="submit" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                                          </a>
+                                          <a href="sp_gig_view.php?spid=' . $sp_id . '&spname=' . $sp_name . '" title="Go to Service gig">
+                                            <button type="submit" class="btn btn-sm btn-theme"><i class="fas fa-eye"></i></button>
+                                          </a>
+                                          </td>
+                                          </tr>';
                                 }
                             }
                             ?>
-
-
                         </tbody>
                         <tfoot>
                             <tr>
@@ -139,68 +110,29 @@ include 'assets/include/admin_header.php';
                                 <th>Phone</th>
                                 <th>City</th>
                                 <th>Status</th>
+                                <th>Operation</th>
                             </tr>
                         </tfoot>
                     </table>
                 </div>
             </div>
-
-            <!--/Datatable-->
-
-
-            <!-- try-start-jsgrid BASIC SCENARIO-->
-            <!-- <div class="row mt-3">
-                <div class="col-sm-12">
-    
-                    <div class="mt-1 mb-4 p-3 button-container bg-white border shadow-sm">
-                        <h6 class="mb-2">Basic Scenario</h6>
-
-                        <div id="jsGrid"></div>
-                    </div>
-                 
-
-                </div>
-            </div> -->
-            <!-- try-end-jsgrid BASIC SCENARIO -->
-
-
         </div>
     </div>
 
-
-
-
-
-
     <!-- Page JavaScript Files-->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/jquery-1.12.4.min.js"></script>
-    <!--Popper JS-->
-    <script src="assets/js/popper.min.js"></script>
-    <!--Bootstrap-->
-    <script src="assets/js/bootstrap.min.js"></script>
-    <!--Sweet alert JS-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="assets/js/sweetalert.js"></script>
-    <!--Progressbar JS-->
     <script src="assets/js/progressbar.min.js"></script>
-    <!--Charts-->
-    <!--Canvas JS-->
     <script src="assets/js/charts/canvas.min.js"></script>
-    <!--Bootstrap Calendar JS-->
     <script src="assets/js/calendar/bootstrap_calendar.js"></script>
     <script src="assets/js/calendar/demo.js"></script>
-    <!--Bootstrap Calendar-->
-    <!--Datatable-->
     <script src="assets/js/jquery.dataTables.min.js"></script>
     <script src="assets/js/dataTables.bootstrap4.min.js"></script>
-    <!--JsGrid table-->
     <script src="assets/js/jsgrid.min.js"></script>
     <script src="assets/js/jsgrid-demo.php"></script>
-
-    <!--Custom Js Script-->
     <script src="assets/js/custom.js"></script>
-    <!--Custom Js Script-->
-
     <script>
         $('#example').DataTable();
     </script>
