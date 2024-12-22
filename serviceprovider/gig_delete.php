b76f2173eae119a84588cf9fd91f3635daf5d443
shahdkammal <shahd.a.kamal.9@gmail.com>
2024-12-22 03:16:23 -0800

    last

--- serviceprovider/gig_delete.php
+++ serviceprovider/gig_delete.php
@@ -12,10 +12,10 @@ if(isset($_GET['service_id'], $_GET['sp_id'])){
     $sql = "DELETE FROM `sp_service` WHERE `sp_service`.`sp_id` = $sp_id AND `service_id` = $service_id";
     $result = mysqli_query($conn,$sql);
     if ($result){
-        $_SESSION['status'] = "Gig deleted successfully.";
+        $_SESSION['status'] = "Service deleted successfully.";
         header("location: gig_view.php");
     }else{
-        $_SESSION['statusfail'] = "Sorry, Gig is not deleted.";
+        $_SESSION['statusfail'] = "Sorry, Service is not deleted.";
         header("location: gig_view.php");
     }
 }
